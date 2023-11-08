4f9d2c3522274ca72e0bfbcda03124a0ec54e59c
Charel Felten <charel.felten@gmail.com>
2023-11-08 18:20:17 +0100

    Initial Commit

---
+++ Tenk/TenkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TenkApp.swift
+//  Tenk
+//
+//  Created by Charel Felten on 08/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct TenkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
